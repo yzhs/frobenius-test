a0e4248d6d0ca9841f97d21f1cc73fd058754cab
Colin Benner <cbyzhs@gmail.com>
2014-02-11 17:30:08 +0100

    Add alternative implementation of Jacobi symbol
    This patch adds the implementation of the Jacoby symbol from PARI (just
    copied the code).  Does not seem to have much influence on performance
    an is thus in a comment for the time being.

--- helpers_int.c
+++ helpers_int.c
@@ -49,7 +49,7 @@ unsigned long split(unsigned long *d, unsigned long n)
 	unsigned long s = 0;
 	*d = n - 1;
 
-	while (*d % 2 == 1) {
+	while (odd(*d)) {
 		s++;
 		*d /= 2;
 	}
@@ -66,6 +66,7 @@ unsigned long get_random(unsigned long n)
 	return (unsigned long)rand() % (n - 3) + 2;
 }
 
+#if 1
 /*
  * Compute the jacobi symbol (x/y) of x over y.  This algorithm is taken from
  * Otto Forster: Algorithmische Zahlentheorie.
@@ -79,7 +80,7 @@ int jacobi(unsigned long x, unsigned long y)
 		x %= y;
 		if (x == 0)
 			return 0;
-		while (x % 2 == 0) {
+		while (even(x)) {
 			x /= 2;
 			m8 = y % 8;
 			if (m8 == 3 || m8 == 5)
@@ -94,6 +95,54 @@ int jacobi(unsigned long x, unsigned long y)
 			res = -res;
 	}
 }
+
+#else
+
+/*
+ * The following implementation of the jacobi function was compied straight
+ * from the soucre code of PARI.
+ */
+
+/*  Compute the 2-adic valuation of z. */
+long vals(unsigned long z)
+{
+  static char tab[64]={-1,0,1,12,2,6,-1,13,3,-1,7,-1,-1,-1,-1,14,10,4,-1,-1,8,-1,-1,25,-1,-1,-1,-1,-1,21,27,15,31,11,5,-1,-1,-1,-1,-1,9,-1,-1,24,-1,-1,20,26,30,-1,-1,-1,-1,23,-1,19,29,-1,22,18,28,17,16,-1};
+  long s;
+
+  if (!z) return -1;
+  if (! (z&0xffffffff)) { s = 32; z >>=32; } else s = 0;
+  z |= ~z + 1;
+  z += z << 4;
+  z += z << 6;
+  z ^= z << 16; /* or  z -= z<<16 */
+  return s + tab[(z&0xffffffff)>>26];
+}
+
+/* Figure out whether t is 3 or 5 modulo 8 */
+#define  ome(t) (labs(((t)&7)-4) == 1)
+
+int jacobi(unsigned long x, unsigned long y)
+{
+	unsigned long z;
+	int s = 1;
+
+	while (x) {
+		long r = vals(x);
+		if (r) {
+			if (odd(r) && ome(y))
+				s = -s;
+			x >>= r;
+		}
+		if (x & y & 2)
+			s = -s;
+		z = y % x;
+		y = x;
+		x = z;
+	}
+
+	return (y == 1) ? s : 0;
+}
+#endif
 
 /*
  * Initialises the random number generator using a (static) seed.
