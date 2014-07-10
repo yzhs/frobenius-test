4f9dc351bc7e68e07049063e7716224199150819
Colin Benner <cbyzhs@gmail.com>
2014-07-10 11:05:39 +0200

    small changes in helper program

--- find_non_smooth_numbers.c
+++ find_non_smooth_numbers.c
@@ -17,8 +17,8 @@ static int has_small_prime_factor(mpz_t n)
 
 int main()
 {
-	unsigned i = 0, p, length;
-	unsigned ints[27];
+	unsigned i = 0, p, length, counter = 0;
+	unsigned ints[50];
 	FILE *fp = fopen("composites.txt", "r");
 	mpz_t n;
 
@@ -34,11 +34,18 @@ int main()
 
 	for (i = 0; i < length; i++) {
 		printf("%u", ints[i]);
+		fflush(stdout);
 		mpz_ui_pow_ui(n, 2, ints[i]);
 		mpz_add_ui(n, n, 1);
 
-		while (mpz_cmp_ui(n, 50000*50000lu) < 0 || has_small_prime_factor(n) || mpz_probab_prime_p(n, 50)) {
+		while (mpz_cmp_ui(n, 50000*50000lu) < 0 || has_small_prime_factor(n) || mpz_probab_prime_p(n, 20)) {
 			mpz_add_ui(n, n, 2);
+			counter++;
+			if (counter >= 10) {
+				fprintf(stderr, ".");
+				fflush(stderr);
+				counter = 0;
+			}
 		}
 		gmp_printf("\t%Zd\n", n);
 	}
