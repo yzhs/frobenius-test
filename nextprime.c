145a8f5b7497f60391d1bd5742e9189c2eb35dce
Colin Benner <colin@yzhs.de>
2014-07-10 11:19:13 +0200

    Add diagnostic output

--- nextprime.c
+++ nextprime.c
@@ -2,23 +2,29 @@
 #include <stdlib.h>
 #include <gmp.h>
 
-#include "common.h"
-
 int main(int argc, char *argv[])
 {
 	unsigned bits;
+	int counter = 0;
 	mpz_t n;
 
-	if (argc < 2)
-		die("Usage: %s <bits>\n", argv[0]);
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <bits>\n", argv[0]);
+		return 1;
+	}
 
 	bits = (unsigned)atoi(argv[1]);
 
 	mpz_init(n);
 	mpz_ui_pow_ui(n, 2, bits);
 	mpz_add_ui(n, n, 1);
-	while (!mpz_probab_prime_p(n, 50))
+
+	while (!mpz_probab_prime_p(n, 25)) {
 		mpz_add_ui(n, n, 2);
+		counter++;
+		if (counter % 200 == 0)
+			fprintf(stderr, "tried %i numbers so far\n", counter);
+	}
 
 	gmp_printf("%Zd\n", n);
 	return 0;
