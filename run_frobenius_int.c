fc85f02b43c9e6fb6a8f40bad21bcaef79eadacb
Colin Benner <colin@yzhs.de>
2014-08-20 11:27:44 +0200

    Remove old machine word Frobenius test testing code

--- run_frobenius_int.c
+++ run_frobenius_int.c
@@ -7,39 +7,6 @@
 
 #define N 1
 
-static void *run(void *worker_id_cast_to_void_star)
-{
-	uint64_t upper_bound = /*1lu << 32*/50000lu*50000+(1lu<<27), dots_every = 1lu << 25;
-	uint64_t counter = 0;
-	unsigned worker_id = (unsigned)(long)worker_id_cast_to_void_star;
-
-#ifdef WRITE_PRIMES
-	char str[32];
-	FILE *primes;
-	(void)snprintf(str, 32, "primes_worker_%lu.txt", worker_id);
-	primes = fopen(str, "a");
-#endif
-
-	for (unsigned i = B * B + 1 + 2 * worker_id; i < upper_bound; i += 2 * N) {
-		if ((i & (dots_every - 1)) == 1) {  /* we need to check for == 1 since i will always be odd */
-			printf(".");
-			(void)fflush(stdout);
-		}
-
-		if (RQFT_int(i, 1)) {
-			counter++;
-#ifdef WRITE_PRIMES
-			fprintf(primes, "%lu\n", i);
-#endif
-		}
-	}
-
-#ifdef WRITE_PRIMES
-	(void)fclose(primes);
-#endif
-	return (void *)counter;
-}
-
 int main()
 {
 	unsigned i;
